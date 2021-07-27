dab8c562ac7b75595793c2c0c7107ebbdf0af51d
eddykang <joji7752@gmail.com>
2021-07-27 17:27:10 +0900

    테스트커밋수정

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div className="footer">
-      <span>Made by.투탕카멘은 피하고 싶어22</span>
+      <span>Made by.투탕카멘은 피하고 싶어</span>
     </div>
   );
 };
