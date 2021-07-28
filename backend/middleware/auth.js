7a2201d545b6367c5e59271be67d51d18a79d80a
limkhl <limkh.dev@gmail.com>
2021-07-29 03:56:37 +0900

    [#10] feat: 로그인, 회원가입 백엔드 구현

---
+++ backend/middleware/auth.js
@@ -0,0 +1,34 @@
+import jwt from "jsonwebtoken";
+import * as userRepository from "../models/user.js";
+import { config } from "../config.js";
+
+const AUTH_ERROR = { message: "인증 에러" };
+
+// 로그인을 요청하는 유저가 가지고 있는 토큰이 유효한 토큰인지 확인하는 내부 미들웨어
+export const isAuth = async (req, res, next) => {
+  // 유저가 jwt 토큰이 필요한 API를 요청할 때, 클라이언트가 Authorization header에 Access token을 담아서 보낸다
+  const authHeader = req.get("Authorization");
+
+  // Authorization header에 값이 없거나 "Bearer"로 시작하지 않으면 Unauthorized 에러 반환
+  if (!(authHeader && authHeader.startsWith("Bearer "))) {
+    return res.status(401).json(AUTH_ERROR);
+  }
+  // Authorization: <인증 타입> <토큰>
+  // (예시) Authorization: Bearer YWxhZGRpbjpvcGVuc2VzYW1l
+  const token = authHeader.split(" ")[1];
+
+  jwt.verify(token, config.jwt.secretKey, async (error, decoded) => {
+    // 1. 토큰의 유효성 확인 : 유효하지 않은 토큰이면 에러 반환
+    if (error) {
+      return res.status(401).json(AUTH_ERROR);
+    }
+    // 2. 디코딩된 토큰 값 속 회원 id가 실제 회원 정보 안에 있는지 확인
+    const user = await userRepository.findById(decoded.id);
+    if (!user) {
+      return res.status(401).json(AUTH_ERROR);
+    }
+    // 유효한 토큰이면 다음 미들웨어에서 해당 회원 정보 조회하기 위해 회원 id를 req.userId 값에 새로 저장한다
+    req.userId = user.id;
+    next();
+  });
+};
---
+++ backend/routes/auth.js
@@ -0,0 +1,31 @@
+import express from "express";
+import "express-async-errors";
+import { body, validationResult } from "express-validator";
+import { validate } from "../middleware/validator.js"; // 유효성 검사 시 에러를 출력하기 위한 내부 미들웨어
+import * as authController from "../controllers/auth.js"; // auth 주요 로직을 담당하는 컨트롤러 연결
+import { isAuth } from "../middleware/auth.js"; // 회원 인증을 위한 내부 미들웨어
+
+const router = express.Router();
+
+// DB 데이터를 바꾸기 직전에 백엔드에서 유효성 검사 추가 실시
+const validateCredential = [
+  body("email").trim().isEmail().withMessage("유효하지 않은 이메일입니다."),
+  body("password")
+    .trim()
+    .isLength({ min: 5 })
+    .withMessage("비밀번호는 5글자 이상 입력해주세요."),
+  validate,
+];
+
+const validateSignup = [
+  ...validateCredential,
+  body("nickname").trim().notEmpty().withMessage("닉네임을 입력해주세요."),
+  validate,
+];
+
+// REST API 구조를 보기 쉽도록 주요 로직은 controller로 분리
+router.post("/signup", validateSignup, authController.signup);
+router.post("/login", validateCredential, authController.login);
+router.get("/me", isAuth, authController.me);
+
+export default router;
