4f3e45570c226f44ed87fc17b0f9c04e388528a2
limkhl <limkh.dev@gmail.com>
2021-08-02 20:29:07 +0900

    [#10] feat: 로그인, 회원가입 프론트 구현

---
+++ frontend/src/pages/LoginPage.js
@@ -0,0 +1,71 @@
+import React, { useCallback } from 'react';
+import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router';
+import useInput from '../hooks/useInput';
+import { loginAction } from '../redux/user';
+import axios from 'axios';
+
+const LoginPage = () => {
+  const dispatch = useDispatch();
+  const [email, onChangeEmail] = useInput('');
+  const [password, onChangePassword] = useInput('');
+  const history = useHistory();
+
+  const onSubmitLogin = useCallback(
+    (e) => {
+      e.preventDefault();
+
+      axios
+        .post('auth/login', { email, password })
+        .then((res) => {
+          if (res.status === 200) {
+            window.localStorage.setItem('token', res.data.token);
+            dispatch(loginAction({ email, password }));
+            alert('로그인 성공 !');
+            history.push('/');
+          } else {
+            alert(res.statusText);
+          }
+        })
+        .catch((err) =>
+          err.response.data.message
+            ? alert(err.response.data.message)
+            : alert(err.response.statusText),
+        );
+    },
+    [email, password],
+  );
+
+  return (
+    <div className="auth-form">
+      <h2>LOGIN</h2>
+      <form onSubmit={onSubmitLogin}>
+        <p>
+          <label htmlFor="email">
+            이메일
+            <br />
+            <input name="email" type="email" value={email} onChange={onChangeEmail} required />
+          </label>
+        </p>
+        <p>
+          <label htmlFor="password">
+            비밀번호
+            <br />
+            <input
+              name="password"
+              type="password"
+              value={password}
+              onChange={onChangePassword}
+              required
+            />
+          </label>
+        </p>
+        <p>
+          <button>로그인</button>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
