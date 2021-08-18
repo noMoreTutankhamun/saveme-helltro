3ec2124510f722c9d9bb0a07526a11112bbf9633
yeongji Cho <joji7752@gmail.com>
2021-08-18 23:41:16 +0900

    style: comment out tagbox

--- frontend/src/pages/WritePage.js
+++ frontend/src/pages/WritePage.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import Responsive from '../components/common/Responsive';
 import Editor from '../components/write/Editor';
-import TagBox from '../components/write/TagBox';
+// import TagBox from '../components/write/TagBox';
 import WriteActionButtons from '../components/write/WriteActionButtons';
 
 const WritePage = () => {
   return (
     <Responsive>
       <Editor></Editor>
-      <TagBox></TagBox>
+      {/* <TagBox></TagBox> */}
       <WriteActionButtons />
     </Responsive>
   );
